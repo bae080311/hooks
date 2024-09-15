ed31718b7061e9eccef491152a31cb9829a36a37
bae080311 <s24024@gsm.hs.kr>
2024-09-15 22:45:56 +0900

    useRef

---
+++ src/useRef.js
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+
+function useClick() {
+  const input = useRef();
+  setTimeout(() => console.log(input.current), 5000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <input ref={input} placeholder="lalala"></input>
+    </div>
+  );
+}
